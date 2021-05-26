57abc235c880dac322035a1005bac9179b384068
M0dix <76493724+M0dix@users.noreply.github.com>
2021-05-26 19:03:38 +0400

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function getWeather(){
     let container = document.getElementById("card_container");
 
     let appid = "09a0e4cda3a57a2711f58769cfd506a3";
-    fetch(`http://api.openweathermap.org/data/2.5/weather?id=${cityarrs[cityvalue]}&appid=${APIkey}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?id=${cityarrs[cityvalue]}&appid=${APIkey}`)
     .then(function(resp){ return resp.json();})
     .then(function(data){ 
         console.log(data);
@@ -17,7 +17,7 @@ function getWeather(){
             <span class="weather_element_name"><p>${data.name.toUpperCase()}</p></span>
             <span class="weather_element_temp"><p>${Math.round(data.main.temp - 273)}&deg</p></span>
             <span class="weather_element_disclaimer"><p>${data.weather[0]['description']}</p></span>
-            <p class="weather_element_img"><img src="http://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">
+            <p class="weather_element_img"><img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">
         </div>
         `
     })
